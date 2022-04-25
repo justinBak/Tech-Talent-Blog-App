1277a304a765930bf061ce6338f78ca6547e5c7e
Justin Baker <100954109+justinBak@users.noreply.github.com>
2022-04-25 12:21:43 -0400

    Add techtalentblog.java

--- techtalentblog/src/main/java/com/techtalentsouth/techtalentblog/blogposts/BlogPostController.java
+++ techtalentblog/src/main/java/com/techtalentsouth/techtalentblog/blogposts/BlogPostController.java
@@ -2,12 +2,15 @@ package com.techtalentsouth.techtalentblog.blogposts;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogPostController {
@@ -19,13 +22,21 @@ public class BlogPostController {
 	
 	@GetMapping(value="/")
 	public String index(Model model) {
+		posts.removeAll(posts);
+		Iterable<BlogPost> iterable = blogPostRepository.findAll();
+		for(BlogPost post : iterable) {
+			posts.add(post);
+		}
+		
+//		Iterator<BlogPost>iterator = iterable.iterator();
+		
 		model.addAttribute("posts", posts);
 		return "blogpost/index";
 	}
 	
 	@GetMapping(value="/blogposts/new")
 	public String newBlog(BlogPost post) {
-		post.setAuthor("Justin");
+//		post.setAuthor("Justin");
 		return "blogpost/new";
 	}
 	
@@ -34,13 +45,39 @@ public class BlogPostController {
 		//This method's goal is to save a blogPost entered in the form to the database
 		BlogPost savedBlogPost = blogPostRepository.save(blogPost);
 		
-		posts.add(savedBlogPost);
-		
-		model.addAttribute("title", savedBlogPost.getTitle());
-		model.addAttribute("author", savedBlogPost.getAuthor());
-		model.addAttribute("blogEntry", savedBlogPost.getBlogEntry());
-		
-		System.out.println("-------" + savedBlogPost.getId());
+		model.addAttribute("blogPost", savedBlogPost);
 		return "blogpost/result";
+	}
+	
+	@GetMapping(value="/blogposts/{id}")
+	public String editPostWithId(@PathVariable Long id, Model model) {
+//		Now we are going to use the id to load the blogpost from the database table that has that id.
+		Optional<BlogPost> post = blogPostRepository.findById(id);
+		if(post.isPresent()) {
+			BlogPost actualPost = post.get();
+			model.addAttribute("blogPost", actualPost);
+		}
+		return "blogpost/edit";
+	}
+	
+	@RequestMapping(value="/blogposts/update/{id}")
+	public String updateExisitingPost(@PathVariable Long id, BlogPost blogPost, Model model) {
+		Optional<BlogPost> post = blogPostRepository.findById(id);
+		
+		if(post.isPresent()) {
+			BlogPost actualPost = post.get();
+			actualPost.setTitle(blogPost.getTitle());
+			actualPost.setAuthor(blogPost.getAuthor());
+			actualPost.setBlogEntry(blogPost.getBlogEntry());
+			blogPostRepository.save(actualPost);
+			model.addAttribute("blogPost", actualPost);
+			}
+		return "blogpost/result";
+	}
+	
+	@RequestMapping(value="/blogposts/delete/{id}")
+	public String deletePostById(@PathVariable Long id) {
+		blogPostRepository.deleteById(id);
+		return "blogpost/delete";
 	}
 }
